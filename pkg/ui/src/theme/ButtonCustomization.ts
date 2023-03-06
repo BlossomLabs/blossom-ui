e319f57200d42f8328f264d10ffc3063c6a2881c
Viviane Dias <vivianedias@pm.me>
2023-03-06 16:36:15 -0300

    feat(btn): adds badge variant for button

--- pkg/ui/src/theme/ButtonCustomization.ts
+++ pkg/ui/src/theme/ButtonCustomization.ts
@@ -1,5 +1,16 @@
 import { defineStyle, defineStyleConfig } from "@chakra-ui/react";
 
+const buttonBase = {
+  whiteSpace: "nowrap",
+  textDecoration: "none",
+  textAlign: "center",
+  background: "none",
+  padding: 0,
+  border: 0,
+  outline: 0,
+  borderRadius: "base",
+};
+
 const customIconButton = defineStyle({
   bgColor: "inherit",
   border: "none",
@@ -13,8 +24,31 @@ const customIconButton = defineStyle({
   },
 });
 
+const sizes = {
+  md: defineStyle({
+    height: 6,
+  }),
+};
+
+const badgeVariant = defineStyle((props) => {
+  const { compact, disabled } = props;
+
+  return {
+    ...buttonBase,
+    color: "badgeContent",
+    background: compact ? "transparent" : "badge",
+    _active: {
+      background: !disabled && compact ? "badgePressed" : "initial",
+    },
+  };
+});
+
 const Button = defineStyleConfig({
-  variants: { customIconButton },
+  variants: { customIconButton, badge: badgeVariant },
+  sizes,
+  defaultProps: {
+    size: "md", // set the default color scheme to purple
+  },
 });
 
 export default Button;
