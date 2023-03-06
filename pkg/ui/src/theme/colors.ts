e671c4f712da7cb56ba9024ce2b25384073c8162
Viviane Dias <vivianedias@pm.me>
2023-03-06 19:26:13 -0300

    feat(theme): creates default colors and textStyles files

---
+++ pkg/ui/src/theme/colors.ts
@@ -0,0 +1,35 @@
+import { colorModeFormatter } from "../utils";
+
+export type DefaultColorsType = Record<
+  string,
+  { default: string; _dark: string }
+>;
+
+const defaultColors: DefaultColorsType = {
+  accent: colorModeFormatter("primary.300", "primary.200"),
+  accentContent: colorModeFormatter("neutral.50", "shades.100"),
+  accentHighlight: colorModeFormatter("primary.200", "primary.300"),
+  accentDisabled: colorModeFormatter("neutral.500", "neutral.300"),
+  background: colorModeFormatter("neutral.50", "shades.50"),
+  badge: colorModeFormatter("blue.50", "blue.600"),
+  badgeContent: colorModeFormatter("blue.800", "white"),
+  badgePressed: colorModeFormatter("gray.50", "blue.500"),
+  border: colorModeFormatter("neutral.200", "neutral.100"),
+  content: colorModeFormatter("neutral.900", "shades.100"),
+  contentSecondary: colorModeFormatter("neutral.500", "neutral.500"),
+  contentDisabled: colorModeFormatter("neutral.400"),
+  control: colorModeFormatter("shades.00"),
+  controlActive: colorModeFormatter("primary.500"),
+  controlBorderActive: colorModeFormatter("primary.200", "primary.300"),
+  controlBorderHighlight: colorModeFormatter("neutral.300", "neutral.200"),
+  hint: colorModeFormatter("neutral.300", "neutral.300"),
+  icon: colorModeFormatter("primary.300", "primary.300"),
+  iconHighlight: colorModeFormatter("primary.300", "primary.700"),
+  link: colorModeFormatter("blue.400", "blue.300"),
+  surface: colorModeFormatter("shades.00", "neutral.50"),
+  surfaceContent: colorModeFormatter("neutral.900", "shades.100"),
+  surfaceContentSecondary: colorModeFormatter("neutral.700", "neutral.700"),
+  surfaceHeader: colorModeFormatter("primary.50", "primary.50"),
+};
+
+export default defaultColors;
---
+++ pkg/ui/src/theme/textStyles.ts
@@ -0,0 +1,74 @@
+const textStyles = {
+  title1: {
+    fontSize: "4xl",
+    fontWeight: "semibold",
+    lineHeight: "base",
+  },
+  title2: {
+    fontSize: "3xl",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  title3: {
+    fontSize: "2xl",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  title4: {
+    fontSize: "xl",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  body1: {
+    fontSize: "lg",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  body2: {
+    fontSize: "md",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  body3: {
+    fontSize: "sm",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  body4: {
+    fontSize: "xs",
+    fontWeight: "normal",
+    lineHeight: "base",
+  },
+  label1: {
+    fontSize: "sm",
+    fontWeight: "normal",
+    lineHeight: "base",
+    textTransform: "uppercase",
+  },
+  label2: {
+    fontSize: "xs",
+    fontWeight: "semibold",
+    lineHeight: "base",
+    textTransform: "uppercase",
+  },
+  label3: {
+    fontSize: "2xs",
+    fontWeight: "normal",
+    lineHeight: "base",
+    textTransform: "uppercase",
+  },
+  address1: {
+    fontSize: "md",
+    fontWeight: "normal",
+    lineHeight: "base",
+    fontFamily: "monospace",
+  },
+  address2: {
+    fontSize: "sm",
+    fontWeight: "normal",
+    lineHeight: "base",
+    fontFamily: "monospace",
+  },
+};
+
+export default textStyles;
