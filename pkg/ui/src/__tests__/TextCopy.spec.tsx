b70feea750664154529dcd1f5201d26d1e884c39
Viviane Dias <vivianedias@pm.me>
2023-03-08 16:12:03 -0300

    test(ui): renames text copy test file with spec extension

--- pkg/ui/src/__tests__/TextCopy.spec.tsx
+++ pkg/ui/src/__tests__/TextCopy.spec.tsx
@@ -1,8 +1,8 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Button from "../components/Button";
+import TextCopy from "../components/TextCopy";
 
-test("Button", () => {
-  render(<Button />);
+test("TextCopy", () => {
+  render(<TextCopy value={"text to be copied"} />);
   expect(screen.getByRole("button")).toBeDefined();
 });
